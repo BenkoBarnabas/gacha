f9503509b6a7d28830b9ff3998b7a0185e99a4cd
BenkoBarnabas <benko.barnus1@gmail.com>
2023-09-08 12:34:19 +0200

    dr tams

--- routes/+page.svelte
+++ routes/+page.svelte
@@ -1,1 +1,20 @@
-<a href="/gachaScreen">sepd ur money here</a>
+<script>
+    import axios from 'axios'
+    var x = 0
+    vary = 1
+    function f() {
+        axios
+            .get(`http://localhost:3000/${x}`)
+            .then(v => x = v.data.x)
+    }
+    function g() {
+        axios
+            .post(`http://localhost:3000/`, {a : x, v : [23, 34]})
+            .then(v => {
+                x = v.data.x
+            })
+    }
+</script>
+<button on:click={f}>Get</button>
+<button on:click={g}>Post</button>
+{x}
