baacfb6002f7e443ad4a027cd4e2dc5839263b50
zseszti0 <zseszti42.3@gmail.com>
2023-09-10 01:04:52 +0200

    WORKING GACHA SYSTEM

--- routes/gachaScreen/+page.svelte
+++ routes/gachaScreen/+page.svelte
@@ -8,10 +8,31 @@
     import rollCard from "../../lib/assets/gacha/rollBg.png"
     import stars from "../../lib/assets/gacha/stars.png"
 
-    var whichBanner //if SN banner active true : false
+    import roll5star from "../../lib/assets/gacha/roll5star.png"
+    import roll4star from "../../lib/assets/gacha/roll4star.png"
+    import roll3star from "../../lib/assets/gacha/roll3star.png"
+
+    var whichBanner = true //if SN banner active true : false
     var isRolling = false
     var pullNum
-    var YourPulls = [3,3,3,3,3,3,3,3,3,3]
+    var yourPulls= Array(10).fill(roll3star);
+    var yourPullsVisibilityClass = [
+    {id: 0, name: 'rollCard', src: rollCard},
+    {id: 1, name: 'rollCard', src: rollCard},
+    {id: 2, name: 'rollCard', src: rollCard},
+    {id: 3, name: 'rollCard', src: rollCard},
+    {id: 4, name: 'rollCard', src: rollCard},
+    {id: 5, name: 'rollCard', src: rollCard},
+    {id: 6, name: 'rollCard', src: rollCard},
+    {id: 7, name: 'rollCard', src: rollCard},
+    {id: 8, name: 'rollCard', src: rollCard},
+    {id: 9, name: 'rollCard', src: rollCard}
+    ];
+
+
+    function wait(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    }
 
     function ActivateBanner(param){
         whichBanner = param
@@ -22,39 +43,53 @@
         isRolling = true
         pullNum = num
 
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 10; i++) {
             if (Math.floor(Math.random() * 10) >= 9){
-                YourPulls[i] = 5
+                yourPulls[i] = roll5star
                 console.log("5 star");
             }
             else if (Math.floor(Math.random() * 10) >= 8){
-                YourPulls[i] = 4
+                yourPulls[i] = roll4star
                 console.log("4 star");
             }
+            DoPullAnimation(i)
         }
+        console.log(yourPulls);
+    }
+
+    function DoPullAnimation(i) {
+        //setTimeout(() => {
+                //yourPullsVisibilityClass[i].name = "rollCardVisible"
+            //}, 1000);
+        (async () => {
+        await wait((i+1)*100);
+        yourPullsVisibilityClass[i].name = "rollCardVisible"
+        })();
     }
 
     function ClosePull(){
         isRolling = false
         isRolling = isRolling
-    }
-
-    function CalculatePulls(index) {
-        console.log(YourPulls[index]);
-        if(YourPulls[index] == 3){
-            rollCard = "../../lib/assets/gacha/roll3star.png"
+        for(let i = 0; i<10; i++){
+            yourPullsVisibilityClass[i].name = "rollCard"
+            yourPullsVisibilityClass[i].src = rollCard
         }
-    }
-    
-</script>
+        yourPulls.fill(roll3star)
 
+    }
+
+    function CalculatePulls(i) {
+        yourPullsVisibilityClass[i].src = yourPulls[i]
+    }
+
+</script>
 {#if isRolling}
     <div id="rollScreen">
         <div id="rollsDiv"></div>
 
         <div id="rollContainer">
-        {#each Array(pullNum) as _, index}
-        <img on:click={() => CalculatePulls(index)} class="rollCard" src={rollCard} alt="rollCard">
+        {#each Array(pullNum) as _,index (yourPullsVisibilityClass[index].id)}
+        <img on:click={() => CalculatePulls(index)} class={yourPullsVisibilityClass[index].name} src={yourPullsVisibilityClass[index].src} alt="rollCard">
         {/each}
         </div>
         
@@ -119,19 +154,69 @@
         width: 80vw;
     }
     #rollsDiv{
-        position: absolute;
         background-color: rgba(44, 46, 67, 0.852);
-        width: 80vw;
-        height: 36vh;
-        left: 10vw;
-        top: 30vh;
+        margin-top: 14vh;
+        margin-left: 10vw;
+        animation: rollsDivAnimation .4s linear forwards;
+        position:absolute;
+        z-index: 0;
     }
+    @keyframes rollsDivAnimation {
+    0% {
+        height: 0vw;
+        width: 2vw;
+        margin-top: 36vh;
+        margin-left: 49vw;
+    }
+    50% {
+        height: 36vh;
+        width: 2vw;
+        margin-top: 14vh;
+        margin-left: 49vw;
+    }
+    100% {
+        height: 36vh;
+        width: 80vw;
+        margin-top: 14vh;
+        margin-left: 10vw;
+    }
+    }
+
     .rollCard{
         width: 6vw;
-        z-index: 3;
         margin: auto;
         padding: 0;
+        opacity: 0;
     }
+    .rollCardVisible {
+        width: 6vw;
+        margin: auto;
+        padding: 0;
+        opacity: 0;
+        animation: pullAnimation .25s linear forwards;
+        animation-delay: .3s;
+        z-index: 2; 
+    }
+
+    @keyframes pullAnimation {
+    0% {
+        height: 0vw;
+        opacity: 0;
+    }
+    30% {
+        height: 16.8627777vw;
+    }
+    100% {
+        height: 33.7255554vw;
+        opacity: 1;
+    }
+    }
+    .rollCardVisible:hover{
+        transform: scale(1.1);
+        filter: hue-rotate(-10deg);
+        cursor: pointer;
+    }
+
 
 
     #banner {
