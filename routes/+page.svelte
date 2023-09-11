3eda0cc5b12de98245e0aa2b825a41d742e43916
zseszti0 <zseszti42.3@gmail.com>
2023-09-11 20:12:24 +0200

    backend test

--- routes/+page.svelte
+++ routes/+page.svelte
@@ -1,74 +1,38 @@
+
 <script>
-    let dataToSend = '';
-    let responseData = '';
-  
-    function sendData() {
-      const data = JSON.stringify({ data: dataToSend });
-  
-      fetch('http://localhost:3000/sendData', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: data,
-      })
-      .then(response => {
-        if (response.ok) {
-          responseData = `Data sent to server: ${dataToSend}`;
-        } else {
-          throw new Error('Failed to send data to server');
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }
-  
-    function getData() {
-      fetch('http://localhost:3000/getData')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Failed to fetch data from server');
-        }
-      })
-      .then(data => {
-        responseData = `Data received from server: ${data.data}`;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }
-  </script>
-  
-  <main>
-    <h1>Svelte App with Node.js and Express.js</h1>
-    <input type="text" bind:value={dataToSend} placeholder="Enter data">
-    <button on:click={sendData}>Send Data to Server</button>
-    <button on:click={getData}>Get Data from Server</button>
-    <p>{responseData}</p>
-  </main>
+  import {sendData, getData } from "../server.js"
+
+  let dataToSend = "";
+  var respons = ""
+  function getData2() {
+    respons = getData()
+  }
+</script>
+
+<main>
+<h1>Svelte App with Node.js and Express.js</h1>
+<input type="text" bind:value={dataToSend} placeholder="Enter data">
+<button on:click={() => sendData(dataToSend)}>Send Data to Server</button>
+<button on:click={() => getData2()}>Get Data from Server</button>
+<p>{respons}</p>
+</main>
 
 
-    <a href="/gachaScreen">sepd ur money here</a>
-  
-  <style>
-    main {
-      text-align: center;
-      padding: 2rem;
-    }
-  
-    h1 {
-      font-size: 1.5rem;
-    }
-  
-    input,
-    button {
-      margin: 1rem;
-      padding: 0.5rem;
-    }
-  </style>
+<a href="/gachaScreen">sepd ur money here</a>
 
+<style>
+main {
+  text-align: center;
+  padding: 2rem;
+}
 
+h1 {
+  font-size: 1.5rem;
+}
 
+input,
+button {
+  margin: 1rem;
+  padding: 0.5rem;
+}
+</style>
