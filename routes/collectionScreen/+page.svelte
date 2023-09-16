6dfb1c513d32102de9f264bde76bdd7ff50d2142
BenkoBarnabas <benko.barnus1@gmail.com>
2023-09-16 11:04:40 +0200

    epic bg

--- routes/collectionScreen/+page.svelte
+++ routes/collectionScreen/+page.svelte
@@ -23,37 +23,39 @@
     let selectedCollection = 1
 
     class diakCards {
-        constructor(name, description, health, attack, source, stars) {
+        constructor(name, description, health, attack, source, stars, cost) {
             this.name = name;
             this.description = description;
             this.health = health;
             this.attack = attack;
             this.source = source;
             this.stars = stars;
+            this.cost = cost;
         }
     }
 
     class tanarCards {
-        constructor(name, description, health, attack, source, stars) {
+        constructor(name, description, health, attack, source, stars, cost) {
             this.name = name;
             this.description = description;
             this.health = health;
             this.attack = attack;
             this.source = source;
             this.stars = stars;
+            this.cost = cost;
         }
     }
-    const BizsoCard = new diakCards("Bizsó", "Kedvence egy jó python feladat. Ha szépen kéred, kienged a hátsó kijáraton. Szexi apuka.", 10, 5, Bizso, 3); 
-    const FarkasCard = new tanarCards("Dr. Farkas", "Filozófusszakállát évente vágja. Ennek ellenére mindig visszanő.", 10, 5, Farkas, 5);
-    const MoniCard = new tanarCards("Móni", "Hobbifilozófus. Képes 45 percig elemezni egy 10 soros lírai alkotást. Lenyűgöző!", 10, 5, Moni, 4);
-    const NagyoraCard = new tanarCards("Big D", "Average tank build. Not much to say.", 10, 5, Nagyora, 3);
-    const RozgonyiCard = new tanarCards("Rozgonyi", "Szeret sütni. Szeret még legózni és gombócot enni is. A fiát láthatólag annyira nem.", 10, 5, Rozgonyi, 3);
-    const TabiCard = new tanarCards("Tábi", "Tesitanár és röplabdaedző. Epic gamer szabadidejében. Sose hagyja ki az alkalmat, hogy politizáljon.", 10, 5, Tabi, 3);
+    const BizsoCard = new diakCards("Bizsó", "Kedvence egy jó python feladat. Ha szépen kéred, kienged a hátsó kijáraton. Szexi apuka.", 9, 5, Bizso, 4, 2); 
+    const FarkasCard = new tanarCards("Dr. Farkas", "Filozófusszakállát évente vágja. Ennek ellenére mindig visszanő.", 17, 8, Farkas, 5, 4);
+    const MoniCard = new tanarCards("Móni", "Hobbifilozófus. Képes 45 percig elemezni egy 10 soros lírai alkotást. Lenyűgöző!", 8, 6, Moni, 4, 2);
+    const NagyoraCard = new tanarCards("Big D", "Average tank build. Not much to say.", 30, 4, Nagyora, 3, 5);
+    const RozgonyiCard = new tanarCards("Rozgonyi", "Szeret sütni. Szeret még legózni és gombócot enni is. A fiát láthatólag annyira nem.", 17, 3, Rozgonyi, 3, 3);
+    const TabiCard = new tanarCards("Tábi", "Tesitanár és röplabdaedző. Epic gamer szabadidejében. Sose hagyja ki az alkalmat, hogy politizáljon.", 6, 7, Tabi, 3, 2);
 
-    const ArhoCard = new diakCards("Arhó", "7 évesen megfejtette a tudományt. Hisz a szabad akaratban.", 10, 5, Arho, 3); 
-    const BarniCard = new diakCards("Barni", "Szeretik a lányok, szeretik a fiúk, ő pedig a krumplis tésztát szereti.", 10, 5, Barni, 3);
-    const EszterCard = new diakCards("Eszter", "Nem tart otthon tyúkokat, de nagyon szeretne. Este 9-kor kizavarják a kollégium gépterméből.", 10, 5, Eszter, 3);
-    const OliviaCard = new diakCards("Olívia", "Lehelt már számtalanszon visszautasította. Oliver ikertestvére. Lehel ezt még nem tudja.", 10, 5, Olivia, 3);
+    const ArhoCard = new diakCards("Arhó", "7 évesen megfejtette a tudományt. Hisz a szabad akaratban.", 12, 6, Arho, 4, 3); 
+    const BarniCard = new diakCards("Barni", "Szeretik a lányok, szeretik a fiúk, ő pedig a krumplis tésztát szereti.", 18, 7, Barni, 3, 4);
+    const EszterCard = new diakCards("Eszter", "Nem tart otthon tyúkokat, de nagyon szeretne. Este 9-kor kizavarják a kollégium gépterméből.", 5, 5, Eszter, 4, 1);
+    const OliviaCard = new diakCards("Olívia", "Lehelt már számtalanszon visszautasította. Oliver ikertestvére. Lehel ezt még nem tudja.", 12, 4, Olivia, 4, 2);
     const ZalanCard = new diakCards("Zalán", "Mindenki kedvenc DÖK elnöke. Volt. Fasza bőrdzseki!", 10, 5, Zalan, 3);
     const ZenoCard = new diakCards("Zénó", "SG DÖK elnök. Ha a terem hátsó sarkában valaki fulladozik a nevetéstől, ő az.", 10, 5, Zeno, 5);
 
@@ -66,12 +68,9 @@
     let cardHealth
     let cardAttack
     let cardSource
-    let cardStars
+    let cardStars = ""
 
     let descriptionPage = false
-    if(descriptionPage){
-        console.log(descriptionPage);
-    }
 
     function handleClick(name, description, health, attack, source, stars) {
         cardName = name
@@ -79,12 +78,17 @@
         cardHealth = health
         cardAttack = attack
         cardSource = source
-        cardStars = stars
+        cardStars = ""
+        
+        for(let i = 0; i < stars; i++){
+            cardStars = cardStars + "*"
+        }
         descriptionPage = true
     }
 </script>
 
-<h1 style="margin-top:0; font-size:5vmin; text-align:center;">Collection</h1>
+<div id = "background"></div>
+<h1 style="margin-top:3vh; font-size:5vmin; text-align:center; color:white">Collection</h1>
 <div id="typeChoosers">
     <!-- buttons to choose the active banner -->
     <button style="background: URL({SNCover}), no-repeat; background-size:cover" class="bannerIcon" on:click={() => selectedCollection = 1}></button>
@@ -101,18 +105,17 @@
 {:else if selectedCollection == 2}
     <div class = "cardcollection" id = "diakcollection">
         {#each diakCardsArr as card}
-            <button class = "cardButton" style="background-image: url({card.source})" on:click={() => handleClick(card.name, card.description)}></button>
+            <button class = "cardButton" style="background-image: url({card.source})" on:click={() => handleClick(card.name, card.description, card.health, card.attack, card.source, card.stars)}></button>
         {/each}
     </div>
 {/if}
 
 {#if descriptionPage}
-    <div></div>
-        <h2>{cardName}</h2>
+    <div class = "bg">
+        <h2>{cardName} {cardStars}</h2>
         <h3>{cardDescription}</h3>
         <p>Health: {cardHealth}</p>
         <p>Attack: {cardAttack}</p>
-        <p>{cardStars} stars</p>
     </div>
 {/if}
 
@@ -133,5 +136,25 @@
     .cardButton:hover{
         cursor:pointer;
         scale:1.02;
+    }
+
+    #background {
+        background-image: url("../../lib/assets/collection/bg.png");
+        overflow: hidden;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: cover; /* Adjust as needed: cover, contain, etc. */
+        background-repeat: no-repeat;
+        background-position: center center;
+        z-index: -3;
+    }
+
+    .bg{
+        background-color: rgba(224, 123, 157, 0.5);
+        padding:10px;
+        margin:10px;
     }
 </style>
